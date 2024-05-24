8794e8592e035b0bc13c72fbd0e47a274f445de0
David Adriazola <d.adriazolamuriel@gmail.com>
2024-05-24 19:36:47 -0400

    feat:validator

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,3 +1,16 @@
+import { IsBoolean, IsNotEmpty, Length, MinLength } from 'class-validator';
 import { OmitType } from '@nestjs/mapped-types';
 import { Todo } from '../entities/todo.entity';
-export class CreateTodoDto extends OmitType(Todo, ['id']) {}
+export class CreateTodoDto extends OmitType(Todo, ['id']) {
+  @IsNotEmpty()
+  @MinLength(5)
+  title: string;
+
+  @IsNotEmpty()
+  @Length(10, 12)
+  description: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  done: boolean;
+}
--- src/todo/dto/update-todo.dto.ts
+++ src/todo/dto/update-todo.dto.ts
@@ -1,4 +1,17 @@
+import { IsBoolean, IsNotEmpty, Length, MinLength } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTodoDto } from './create-todo.dto';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {
+  @IsNotEmpty()
+  @MinLength(5)
+  title: string;
+
+  @IsNotEmpty()
+  @Length(10, 12)
+  description: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
+  done: boolean;
+}
