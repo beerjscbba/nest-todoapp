eeb7dd4237047aa4b3a915cb537d64f4e1f47582
David Adriazola <d.adriazolamuriel@gmail.com>
2024-05-24 19:50:16 -0400

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
+
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
