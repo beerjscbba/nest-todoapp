41aa174954cbc9bc5bda16a54937e3affca412f3
David Adriazola <d.adriazolamuriel@gmail.com>
2024-05-24 21:02:10 -0400

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [TodoModule],
+  imports: [TodoModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
